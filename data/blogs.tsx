22e5afb700525e16719d9a54ad5e1ff163d2a02c
AhmedZakaria14 <1.95773611e+08+AhmedZakaria14@users.noreply.github.com>
2026-08-22 13:13:39 +0000

    Replace blog covers with branded contact design

--- data/blogs.tsx
+++ data/blogs.tsx
@@ -14,7 +14,7 @@ export interface BlogPost {
   content?: ReactNode;
 }
 
-const DefImage = 'https://res.cloudinary.com/dxvjqrb9l/image/upload/v1783750769/%D9%85%D9%86%D8%AF%D9%88%D8%A8_%D8%B2%D9%8A%D9%86_5g_%D8%A7%D8%AE%D8%B1_%D8%AA%D8%AD%D8%AF%D9%8A%D8%AB_gcjlva.png';
+const DefImage = '/images/blog-cover-zain-contact.png';
 
 export const blogPosts: BlogPost[] = [
   {
--- data/newBlogs.tsx
+++ data/newBlogs.tsx
@@ -1,6 +1,6 @@
 import type { BlogPost } from './blogs';
 
-const DefImage = "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1783750769/%D9%85%D9%86%D8%AF%D9%88%D8%A8_%D8%B2%D9%8A%D9%86_5g_%D8%A7%D8%AE%D8%B1_%D8%AA%D8%AD%D8%AF%D9%8A%D8%AB_gcjlva.png";
+const DefImage = "/images/blog-cover-zain-contact.png";
 
 export const newBlogPosts: BlogPost[] = [
   {
